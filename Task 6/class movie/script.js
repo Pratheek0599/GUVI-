980ee906bf0a885caef7f104ed8b0f90580c646d
Pratheek0599 <pratheek0599@gmail.com>
2024-06-03 17:49:06 +0530

    First commit

---
+++ Task 6/class movie/script.js
@@ -0,0 +1,24 @@
+
+
+class movie {
+  constructor(title, studio, rating) {
+    this.title = title;
+    this.studio = studio;
+    this.rating = rating;
+  }
+
+  getrating() {
+    return "the rating is  " + this.rating;
+  }
+}
+
+class movie {
+  constructor(title, studio, rating) {
+    this.title = title;
+    this.studio = studio;
+    this.rating = "PG";
+  }
+}
+
+
+const CasinoRoyale = new movie( "Casino Royal", "Eon Productions", "PG13");
